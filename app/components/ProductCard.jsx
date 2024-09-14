170284040c86305e8737771b595daa2a7c145ff5
phillip-tech <pm.bogopane@gmail.com>
2024-09-14 02:34:50 +0200

    Refactored :ProductCard.

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Star, ShoppingCart, ChevronLeft, ChevronRight } from 'lucide-react';
