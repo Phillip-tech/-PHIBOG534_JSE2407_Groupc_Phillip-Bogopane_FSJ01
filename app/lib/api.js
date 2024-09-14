826182b5739282dd246c14df41de236db2fc9af4
phillip-tech <pm.bogopane@gmail.com>
2024-09-14 02:36:06 +0200

    "Updated API URL and added getTotalProducts function; removed page.js file; updated ITEMS_PER_PAGE value in products/page.jsx"

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,5 +1,5 @@
-const API_URL = 'https://next-ecommerce-api.vercel.app/products?skip=40' 
- //'https://next-ecommerce-api.vercel.app';
+const API_URL = 'https://next-ecommerce-api.vercel.app' 
+ //' https://next-ecommerce-api.vercel.app/products?skip=40';
 
 export async function getProducts(page = 1, limit = 20, skip = 0) {
   console.log(`Fetching products from ${API_URL}/products?_page=${page}&_limit=${limit}&_start=${skip}`);
@@ -24,3 +24,14 @@ export async function getProduct(id) {
     return product;
   }
   
+
+  export async function getTotalProducts() {
+    console.log(`Fetching total products count from ${API_URL}/products`);
+    const res = await fetch(`${API_URL}/products`, { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error(`Failed to fetch total products. Status: ${res.status}`);
+    }
+    const products = await res.json();
+    console.log(`Total products count: ${products.length}`);
+    return products.length;
+  }
