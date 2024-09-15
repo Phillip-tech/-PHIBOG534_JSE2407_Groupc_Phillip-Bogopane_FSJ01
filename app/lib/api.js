f8615dfbf012a635a971f5f0fd0dfde2f62fe457
phillip-tech <pm.bogopane@gmail.com>
2024-09-15 10:12:47 +0200

    Update API URL and parameters in api.js,

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://next-ecommerce-api.vercel.app' 
- //' https://next-ecommerce-api.vercel.app/products?skip=40';
+const API_URL ='https://next-ecommerce-api.vercel.app/products?limit=100';
+ 
 
-export async function getProducts(page = 1, limit = 20, skip = 0) {
+export async function getProducts(page = 1, limit = 100, skip = 1) {
   console.log(`Fetching products from ${API_URL}/products?_page=${page}&_limit=${limit}&_start=${skip}`);
   const res = await fetch(`${API_URL}/products?_page=${page}&_limit=${limit}&_start=${skip}`, { cache: 'no-store' });
   if (!res.ok) {
@@ -14,24 +14,27 @@ export async function getProducts(page = 1, limit = 20, skip = 0) {
 
 
 export async function getProduct(id) {
-    console.log(`Fetching product with id ${id} from ${API_URL}/products/${id}`);
-    const res = await fetch(`${API_URL}/products/${id}`, { cache: 'no-store' });
-    if (!res.ok) {
+  const singleProductURL = `https://next-ecommerce-api.vercel.app/products/${id}`;
+  console.log(`Fetching product with id ${id} from ${singleProductURL}`);
+  const res = await fetch(singleProductURL, { cache: 'no-store' });
+  if (!res.ok) {
       throw new Error(`Failed to fetch product. Status: ${res.status}`);
-    }
-    const product = await res.json();
-    console.log(`Fetched product:`, product);
-    return product;
   }
+  const product = await res.json();
+  console.log(`Fetched product:`, product);
+  return product;
+}
   
 
-  export async function getTotalProducts() {
-    console.log(`Fetching total products count from ${API_URL}/products`);
-    const res = await fetch(`${API_URL}/products`, { cache: 'no-store' });
-    if (!res.ok) {
-      throw new Error(`Failed to fetch total products. Status: ${res.status}`);
-    }
-    const products = await res.json();
-    console.log(`Total products count: ${products.length}`);
-    return products.length;
-  }
+  // export async function getTotalProducts() {
+  //   console.log(`Fetching total products count from ${API_URL}/products`);
+  //   const res = await fetch(`${API_URL}/products`, { cache: 'no-store' });
+  //   if (!res.ok) {
+  //     throw new Error(`Failed to fetch total products. Status: ${res.status}`);
+  //   }
+  //   const products = await res.json();
+  //   console.log(`Total products count: ${products.length}`);
+  //   return products.length;
+  // }
+
+ 
